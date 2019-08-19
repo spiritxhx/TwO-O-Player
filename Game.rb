80981a47702a10f0b7452a287b4c0e6c1a75f32a
spiritxhx <spiritxhx@gmail.com>
2019-08-19 13:31:20 -0400

    complete the game function

--- Game.rb
+++ Game.rb
@@ -5,24 +5,33 @@ class Game
 
   def game_start(player1, player2)
     i=0
-    while player1.score && player2.score do
+    while (player1.score>0 && player2.score>0) do
       question = Question.new()
       puts "----- New TURN -----"
       puts "Player #{i%2 +1}: #{question.question} "
-      i = i+1
       ans = gets.chomp()
       puts ans
       if (ans.to_i == question.answer)
         puts "Player #{i%2 +1}: YES! You are correct. "
       else
         puts "Player #{i%2 +1}: Seriously? No! "
-        
+        if (i%2==0)
+          player1.score=player1.score-1
+        else
+          player2.score=player2.score-1
+        end
       end
+      
+      puts "P1: #{player1.score}/3 vs P2: #{player2.score}/3 "
+      i = i+1
     end
-  end
-
-
-  def game_over
+    if (player1.score==0)
+      puts "Player 2 wins with a score of #{player2.score}/3 "
+    elsif (player2.score==0)
+      puts "Player 1 wins with a score of #{player1.score}/3 "
+    end
+    puts "----- GAME OVER -----"
+    puts "Good bye!"
 
   end
 end
--- main.rb
+++ main.rb
@@ -5,7 +5,6 @@ require './Game.rb'
 player_1 = Player.new()
 player_2 = Player.new()
 
-question_1 = Question.new()
 
 # puts "#{player_1.score}/3"
 # puts question_1.question
