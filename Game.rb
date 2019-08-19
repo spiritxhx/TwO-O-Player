669f5a87b1ec4b3b45e44c6b2eaeb11f2deb2c5f
spiritxhx <spiritxhx@gmail.com>
2019-08-19 11:46:02 -0400

    add the basic game input to the Game class

--- Game.rb
+++ Game.rb
@@ -1,3 +1,22 @@
+require './Question.rb'
+
 class Game
 
+  def initialize
+    i=0
+  end
+
+
+  def game_start(player1, player2)
+    while player1.score && player2.score do
+      question = Question.new()
+      puts "----- New TURN -----"
+      puts "Player 1: #{question.question} "
+    end
+  end
+
+
+  def game_over
+
+  end
 end
--- main.rb
+++ main.rb
@@ -1,11 +1,15 @@
 require './player.rb'
 require './Question.rb'
+require './Game.rb'
 
 player_1 = Player.new()
 player_2 = Player.new()
 
 question_1 = Question.new()
 
-puts "#{player_1.score}/3"
-puts question_1.question
-puts question_1.answer
+# puts "#{player_1.score}/3"
+# puts question_1.question
+# puts question_1.answer
+
+game = Game.new()
+game.game_start(player_1, player_2)
