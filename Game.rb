9375810bca510a16a0c6feacb624c6b1d28b446e
spiritxhx <spiritxhx@gmail.com>
2019-08-19 13:30:34 -0400

    make sure the score works

--- Game.rb
+++ Game.rb
@@ -2,16 +2,22 @@ require './Question.rb'
 
 class Game
 
-  def initialize
-    i=0
-  end
-
 
   def game_start(player1, player2)
+    i=0
     while player1.score && player2.score do
       question = Question.new()
       puts "----- New TURN -----"
-      puts "Player 1: #{question.question} "
+      puts "Player #{i%2 +1}: #{question.question} "
+      i = i+1
+      ans = gets.chomp()
+      puts ans
+      if (ans.to_i == question.answer)
+        puts "Player #{i%2 +1}: YES! You are correct. "
+      else
+        puts "Player #{i%2 +1}: Seriously? No! "
+        
+      end
     end
   end
 
