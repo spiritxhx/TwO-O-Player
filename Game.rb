6bfc53526f775615f0980f26945a71ed7113df5e
spiritxhx <spiritxhx@gmail.com>
2019-08-19 14:01:52 -0400

    refactor a bit wrap the logic inside individual class

--- Game.rb
+++ Game.rb
@@ -5,29 +5,30 @@ class Game
 
   def game_start(player1, player2)
     i=0
-    while (player1.score>0 && player2.score>0) do
+    while (!player1.dead? && !player2.dead?) do
       question = Question.new()
       puts "----- New TURN -----"
       puts "Player #{i%2 +1}: #{question.question} "
       ans = gets.chomp()
-      puts ans
       if (ans.to_i == question.answer)
         puts "Player #{i%2 +1}: YES! You are correct. "
       else
         puts "Player #{i%2 +1}: Seriously? No! "
         if (i%2==0)
-          player1.score=player1.score-1
+          player1.remove_life
         else
-          player2.score=player2.score-1
+          player2.remove_life
         end
       end
       
       puts "P1: #{player1.score}/3 vs P2: #{player2.score}/3 "
       i = i+1
     end
-    if (player1.score==0)
+
+
+    if (player1.dead?)
       puts "Player 2 wins with a score of #{player2.score}/3 "
-    elsif (player2.score==0)
+    elsif (player2.dead?)
       puts "Player 1 wins with a score of #{player1.score}/3 "
     end
     puts "----- GAME OVER -----"
--- player.rb
+++ player.rb
@@ -5,4 +5,16 @@ class Player
     @score=3
   end
 
+  def remove_life
+    self.score = self.score - 1
+  end
+
+  def dead?
+    if self.score==0
+      return true
+    else
+      return false
+    end
+  end
+
 end
